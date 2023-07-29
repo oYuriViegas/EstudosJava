ccd72bf0644d26f77f9157e52ec21c4e256aac2c
oYuriViegas <yuri.viegas9@gmail.com>
2023-07-29 11:32:18 -0300

    Aprendendo processamento de dados e Casting

--- first_project/src/Main.java
+++ first_project/src/Main.java
@@ -1,12 +1,21 @@
 public class Main {
     public static void main(String[] args){
-        double b, B, h, area;
-        b = 6;
-        B = 8;
-        h = 5;
+        int a, b;
+        double resultado;
 
-        area = ((b+B)/2)*h;
+        a = 5;
+        b = 2;
 
-        System.out.printf("A area do trapézio é %.2f%n", area);
+        resultado = (double) a / b;
+
+        System.out.println(resultado);
+
+        int x;
+        double y;
+
+        y = 5.0;
+        x = (int) y;
+
+        System.out.println(x);
     }
 }
