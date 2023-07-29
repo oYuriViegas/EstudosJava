ee4392e51ffc72f58d307592600d9e1b15f14bc4
oYuriViegas <yuri.viegas9@gmail.com>
2023-07-29 11:24:30 -0300

    fazendo exercício para calcular a area de um trapézio.

--- first_project/src/Main.java
+++ first_project/src/Main.java
@@ -1,8 +1,12 @@
 public class Main {
     public static void main(String[] args){
-        String nome = "Nina";
-        int idade = 20;
-        double salario = 5432.12;
-        System.out.printf("%s tem %d anos e ganha R$ %.2f reais%n", nome, idade, salario);
+        double b, B, h, area;
+        b = 6;
+        B = 8;
+        h = 5;
+
+        area = ((b+B)/2)*h;
+
+        System.out.printf("A area do trapézio é %.2f%n", area);
     }
 }
