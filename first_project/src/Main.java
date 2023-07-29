759b4670f3563b9a96955c41f91fd18bf4aedfdc
oYuriViegas <yuri.viegas9@gmail.com>
2023-07-29 11:08:38 -0300

    first commit

---
+++ first_project/src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args){
+        String nome = "Nina";
+        int idade = 20;
+        double salario = 5432.12;
+        System.out.printf("%s tem %d anos e ganha R$ %.2f reais%n", nome, idade, salario);
+    }
+}
