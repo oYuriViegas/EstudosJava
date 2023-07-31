6707f821ebc2d6f6247c21631f33e124fc966393
Yuri Viegas <yuri.viegas9@gmail.com>
2023-07-31 16:51:19 -0300

    Aprendendendo a ler variaveis na mesma linha

--- first_project/src/Main.java
+++ first_project/src/Main.java
@@ -1,16 +1,27 @@
 import java.util.Scanner;
+import java.util.Locale;
 
-public class Main {
+public class Main{
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
+        Locale.setDefault(Locale.US);
 
         String x;
-
-        System.out.print("Insira um numero: ");
+        int y;
+        double z;
+        char c;
 
         x = sc.next();
+        y = sc.nextInt();
+        z = sc.nextDouble();
+        c = sc.next().charAt(0);
 
-        System.out.println("O número inserido é: " + x);
+        System.out.println("Dados Digitados:");
+
+        System.out.println(x);
+        System.out.println(y);
+        System.out.println(z);
+        System.out.println(c);
 
         sc.close();
     }
