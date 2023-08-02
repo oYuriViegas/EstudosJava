46f65e36bfba2da165497366f4ffdae45966913e
Yuri Viegas <yuri.viegas9@gmail.com>
2023-08-01 22:22:22 -0300

    Calculadora simples em java

--- first_project/src/Main.java
+++ first_project/src/Main.java
@@ -1,28 +1,56 @@
 import java.util.Scanner;
-import java.util.Locale;
 
-public class Main{
-    public static void main(String[] args){
-        Scanner sc = new Scanner(System.in);
-        Locale.setDefault(Locale.US);
+public class CalculadoraSimples {
 
-        String x;
-        int y;
-        double z;
-        char c;
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
 
-        x = sc.next();
-        y = sc.nextInt();
-        z = sc.nextDouble();
-        c = sc.next().charAt(0);
+        System.out.println("Calculadora Simples");
+        System.out.print("Digite o primeiro número: ");
+        double num1 = scanner.nextDouble();
 
-        System.out.println("Dados Digitados:");
+        System.out.print("Digite o segundo número: ");
+        double num2 = scanner.nextDouble();
 
-        System.out.println(x);
-        System.out.println(y);
-        System.out.println(z);
-        System.out.println(c);
+        System.out.println("Escolha a operação (digite o símbolo correspondente):");
+        System.out.println("+ : Adição");
+        System.out.println("- : Subtração");
+        System.out.println("* : Multiplicação");
+        System.out.println("/ : Divisão");
 
-        sc.close();
+        char operacao = scanner.next().charAt(0);
+
+        double resultado = 0.0;
+        boolean operacaoValida = true;
+
+        switch (operacao) {
+            case '+':
+                resultado = num1 + num2;
+                break;
+            case '-':
+                resultado = num1 - num2;
+                break;
+            case '*':
+                resultado = num1 * num2;
+                break;
+            case '/':
+                if (num2 != 0) {
+                    resultado = num1 / num2;
+                } else {
+                    System.out.println("Não é possível dividir por zero.");
+                    operacaoValida = false;
+                }
+                break;
+            default:
+                System.out.println("Operação inválida.");
+                operacaoValida = false;
+                break;
+        }
+
+        if (operacaoValida) {
+            System.out.println("O resultado da operação é: " + resultado);
+        }
+
+        scanner.close();
     }
 }
