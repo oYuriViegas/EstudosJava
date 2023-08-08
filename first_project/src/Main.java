f4ce067357531d02ca6e65f0d1c4b4cc356caecc
oYuriViegas <yuri.viegas9@gmail.com>
2023-08-08 09:42:26 -0300

    Adicionando a opção de inputs do usuarios, na calculadora de bhaskara

--- first_project/src/Main.java
+++ first_project/src/Main.java
@@ -5,18 +5,21 @@ public class Main {
         Scanner sc = new Scanner(System.in);
 
         double delta, a, b, c, x1, x2;
-
-        a = 1.0;
-        b = -3.0;
-        c = -54.0;
+        
+        System.out.println("Insira o valor de \"A\": ");
+        a = sc.nextDouble();
+        System.out.println("Insira o valor de \"B\": ");
+        b = sc.nextDouble();
+        System.out.println("Insira o valor de \"C\": ");
+        c = sc.nextDouble();
 
         delta = Math.pow(b, 2.0) -4*a*c;
 
         x1 = (-b + Math.sqrt(delta)) / (2.0 * a);
         x2 = (-b - Math.sqrt(delta)) / (2.0 * a);
         
-        System.out.println(x1);
-        System.out.println(x2);
+        System.out.printf("x1: %f%n", x1);
+        System.out.printf("x2: %f", x2);
         
         sc.close();
     }
