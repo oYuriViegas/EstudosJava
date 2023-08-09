a68831979fa558d11e9a5bdb75b19e9941bc0ce5
Yuri Viegas <yuri.viegas9@gmail.com>
2023-08-09 18:10:10 -0300

    Programa que lê senha válida

--- first_project/src/Main.java
+++ first_project/src/Main.java
@@ -1,51 +1,28 @@
+// Programa que lê senha válida.
 import java.util.Scanner;
 
 public class Main{
-	public static void main(String[] args) {
+	public static void main(String[] args){
 		Scanner sc = new Scanner(System.in);
-		
-		int opt, amnt;
-		double total = 0;
-		
-		System.out.println("=========== ARTHUR LANCHES ===========");
-		System.out.println("1. Cachorro Quente			R$4.00");
-		System.out.println("2. X-Salada				R$4.50");
-		System.out.println("3. X-Bacon				R$5.00");
-		System.out.println("4. Torrada Simples			R$2.00");
-		System.out.println("5. Refrigerante				R$1.50");
-		
-		System.out.print("Insira o item que deseja: ");
-		opt = sc.nextInt();
-		System.out.print("Insira a quantidade: ");
-		amnt = sc.nextInt();
-		
-		
-		switch(opt) {
-			case 1:
-				total = (float) amnt * 4.00;
-				break;
-			case 2:
-				total = (float) amnt * 4.50;
-				break;
-			case 3:
-				total = (float) amnt * 5.00;
-				break;
-			case 4:
-				total = (float) amnt * 2.00;
-				break;
-			case 5:
-				total = (float) amnt * 1.50;
-				break;
-			default:
-				System.out.println("Opção invalida!!");
+
+		int pw = 2506;
+		int resp = 0;
+
+		while(resp != pw){
+			System.out.print("Insira a senha do sistema: ");
+			resp = sc.nextInt();
+
+			String result = (resp == pw) ? "Acesso liberado!!": "Acesso Negado!!";
+
+			System.out.println(result);
+
+			/* if(resp == pw){
+				System.out.println("Acesso liberado!!");
+			} else {
+				System.out.println("Acesso negado!!");
+			} */
 		}
-		
-		System.out.printf("Total da compra:  R$ %.2f", total);
-		
-        sc.close();
+
+		sc.close();
 	}
 }
-
-
-
-
