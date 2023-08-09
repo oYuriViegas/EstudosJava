b6bbcaa6dabef2e79528f5ea66eff141987120e9
oYuriViegas <yuri.viegas9@gmail.com>
2023-08-09 16:15:49 -0300

    Fazendo exercício de condicionais, ARTHUR LANCHES

--- first_project/src/Main.java
+++ first_project/src/Main.java
@@ -1,26 +1,50 @@
 import java.util.Scanner;
 
-public class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-
-        double delta, a, b, c, x1, x2;
-        
-        System.out.println("Insira o valor de \"A\": ");
-        a = sc.nextDouble();
-        System.out.println("Insira o valor de \"B\": ");
-        b = sc.nextDouble();
-        System.out.println("Insira o valor de \"C\": ");
-        c = sc.nextDouble();
-
-        delta = Math.pow(b, 2.0) -4*a*c;
-
-        x1 = (-b + Math.sqrt(delta)) / (2.0 * a);
-        x2 = (-b - Math.sqrt(delta)) / (2.0 * a);
-        
-        System.out.printf("x1: %f%n", x1);
-        System.out.printf("x2: %f", x2);
-        
-        sc.close();
-    }
+public class Main{
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int opt, amnt;
+		double total = 0;
+		
+		System.out.println("=========== ARTHUR LANCHES ===========");
+		System.out.println("1. Cachorro Quente			R$4.00");
+		System.out.println("2. X-Salada				R$4.50");
+		System.out.println("3. X-Bacon				R$5.00");
+		System.out.println("4. Torrada Simples			R$2.00");
+		System.out.println("5. Refrigerante				R$1.50");
+		
+		System.out.print("Insira o item que deseja: ");
+		opt = sc.nextInt();
+		System.out.print("Insira a quantidade: ");
+		amnt = sc.nextInt();
+		
+		
+		switch(opt) {
+			case 1:
+				total = (float) amnt * 4.00;
+				break;
+			case 2:
+				total = (float) amnt * 4.50;
+				break;
+			case 3:
+				total = (float) amnt * 5.00;
+				break;
+			case 4:
+				total = (float) amnt * 2.00;
+				break;
+			case 5:
+				total = (float) amnt * 1.50;
+				break;
+			default:
+				System.out.println("Opção invalida!!");
+		}
+		
+		System.out.printf("Total da compra:  R$ %.2f", total);
+		
+	}
 }
+
+
+
+
