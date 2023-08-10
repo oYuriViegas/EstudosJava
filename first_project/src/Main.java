a4dc6fac2d36e1eeda1d434785199881f44af1fa
oYuriViegas <yuri.viegas9@gmail.com>
2023-08-10 15:39:18 -0300

    Exercício usando loop for (mostrar os numeros ímpares de 1 até X)

--- first_project/src/Main.java
+++ first_project/src/Main.java
@@ -1,28 +1,26 @@
-// Programa que lê senha válida.
 import java.util.Scanner;
 
 public class Main{
-	public static void main(String[] args){
+	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-
-		int pw = 2506;
-		int resp = 0;
-
-		while(resp != pw){
-			System.out.print("Insira a senha do sistema: ");
-			resp = sc.nextInt();
-
-			String result = (resp == pw) ? "Acesso liberado!!": "Acesso Negado!!";
-
-			System.out.println(result);
-
-			/* if(resp == pw){
-				System.out.println("Acesso liberado!!");
-			} else {
-				System.out.println("Acesso negado!!");
-			} */
+		
+		System.out.println("INICIO DO PROGRAMA");
+		System.out.println("Insira um valor entre 1 e 1000: ");
+		
+		int userNum = sc.nextInt();
+		
+		for(int i = 1; i <= userNum; i++) {
+			if(i % 2 == 1) {
+				System.out.println(i);
+			}
 		}
-
+		
+		System.out.println("Numero do usuario: " + userNum);
+		
+		
+		
+		
+		
 		sc.close();
 	}
 }
