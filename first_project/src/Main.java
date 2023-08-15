b6ce3479c908fafcaa903dce3addd3b7a9ad7ac7
oYuriViegas <yuri.viegas9@gmail.com>
2023-08-15 09:21:52 -0300

    Exercício em aula ##PENDENTE

--- first_project/src/Main.java
+++ first_project/src/Main.java
@@ -1,1 +1,19 @@
-import java.util.Scanner;public class Main{public static void main(String[] args){Scanner sc = new Scanner(System.in);int n = sc.nextInt();for(int i = 1; i <= n; i++) System.out.printf("%d %d %d %n", i, (int)Math.pow(i, 2), (int)Math.pow(i, 3));sc.close();}}
+public class Main
+{
+    public static void main (String[] args)
+    {
+        try{
+            Data descobrimento = new Data ((byte)22,(byte)4,(short)1500);
+            Data tiradentes    = new Data ((byte)21,(byte)4,(short)1792);
+            Data independencia = new Data ((byte)7,(byte)9,(short)1822);
+            
+            System.out.println("Descobrimento: " + descobrimento.getDia() + "/" + descobrimento.getMes() + "/" + descobrimento.getAno());
+            System.out.println("Tiradentes: " + tiradentes.getDia() + "/" + tiradentes.getMes() + "/" + tiradentes.getAno());
+            System.out.println("Independencia: " + independencia.getDia() + "/" + independencia.getMes() + "/" + independencia.getAno());
+        } 
+        catch(Exception error)
+        {
+            System.out.println(error.getMessage());
+        } 
+    }
+}
