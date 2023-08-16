1ba187c3cd0b9c65bd744d7237c88b21d74274c6
Yuri Viegas <yuri.viegas9@gmail.com>
2023-08-16 18:59:24 -0300

    Exercício para controle de estoque simples com OOP

--- course/src/application/Program.java
+++ course/src/application/Program.java
@@ -1,46 +1,43 @@
 package application;
 
+import java.util.Locale;
 import java.util.Scanner;
-import entities.Triangle;
+import entities.Product;
 
 public class Program {
-
 	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		
-		Triangle x, y;
-		x = new Triangle();
-		y = new Triangle();
-		
-		
-		System.out.println("Enter the measures of triangle X: ");
-		x.a = sc.nextDouble();
-		x.b = sc.nextDouble();
-		x.c = sc.nextDouble();
-		System.out.println("Enter the measures of triangle Y: ");
-		y.a = sc.nextDouble();
-		y.b = sc.nextDouble();
-		y.c = sc.nextDouble();
-		
-		double p = (x.a + x.b + x.c) / 2;
-		double areaX = Math.sqrt(p * (p - x.a) * (p - x.b) * (p - x.c));
-		
-		p = (y.a + y.b + y.c) / 2.0;
-		double areaY = Math.sqrt(p * (p - y.a) * (p - y.b) * (p - y.c));
-		
-		
-		System.out.printf("Triangle X area: %.4f%n", areaX);
-		System.out.printf("Triangle Y area: %.4f%n", areaY);
-		
-		if(areaX > areaY) {
-			System.out.println("Larger area: X");
-		}
-		else {
-			System.out.println("Larger area: Y");
-		}
-		
-		
-		sc.close();
+			Locale.setDefault(Locale.US);
+			Scanner sc = new Scanner(System.in);
+			
+			Product product = new Product();
+			
+			System.out.println("Enter product data: ");
+			System.out.print("Name: ");
+			product.name = sc.nextLine();
+			System.out.print("Price: ");
+			product.price = sc.nextDouble();
+			System.out.print("Quantity in stock: ");
+			product.quantity = sc.nextInt();
+			
+			System.out.println();
+			System.out.println("Product data: " + product);
+			
+			System.out.println();
+			System.out.print("Enter the number of products to be added in stock: ");
+			int quantity = sc.nextInt();
+			product.addProducts(quantity);
+			
+			System.out.println();
+			System.out.println("Updated data: " + product);
+			
+			System.out.println();
+			System.out.print("Enter the number of products to be removed from stock: ");
+			quantity = sc.nextInt();
+			product.removeProducts(quantity);
+			
+			System.out.println();
+			System.out.println("Updated data: " + product);
+			
+			sc.close();
 	}
-
 }
